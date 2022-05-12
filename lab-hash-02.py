eb49fefdc0960f49f9052937d2749062c9035577
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2022-05-12 00:35:42 -0500

    HASH 02 FIX.

--- lab-hash-02.py
+++ lab-hash-02.py
@@ -6,26 +6,46 @@
 # DIEGO ALEJANDRO POVEDA SANCHEZ - 10892217137
 # ----------------------------------------------------------------------------------------------------
 import hashlib
+import unicodedata
 
-encoding = 'utf-16'
+ENCODING = 'utf-16'
+
 
 def hash_with_md5(text):
-    value = text.encode(encoding)
+    value = text.encode(ENCODING)
     hash_value = hashlib.md5(value).hexdigest()
-    print(value.decode(encoding) + " (" + str(len(value)) + "): " + hash_value)
+    print(str(value) + " (" + str(len(value)) + "): " + hash_value)
     return hash_value
 
+
+def replace_weird_characters(text):
+    result = []
+    for c in text:
+        try:
+            unicodedata.name(c)
+            result.append(str(c))
+        except ValueError:
+            result.append("?")
+    return ''.join(result)
+
+
 def found_hash(hash_value):
-    current_value = ""
     min_range = 0
-    max_range = 255
+    max_range = 256
     for byte1 in range(min_range, max_range):
         for byte2 in range(min_range, max_range):
             for byte3 in range(min_range, max_range):
-                current_value = str(chr(byte1)) + str(chr(byte2)) + str(chr(byte3))
+                current_value = str(chr(byte1)) + \
+                    str(chr(byte2)) + str(chr(byte3))
                 found_hash = hash_with_md5(current_value)
                 if found_hash == hash_value:
-                    print("ENCONTRO\t=[" + current_value + "]\t" + found_hash)
+                    print("ENCONTRO\t"
+                          + found_hash
+                          + "\t[" + str(byte1) + "][" +
+                          str(byte2) + "][" + str(byte3) + "]\t"
+                          + replace_weird_characters(current_value)
+                          )
                     exit()
+
 
 found_hash("e96a036be3be000fefc0ebc061fb3b11")
